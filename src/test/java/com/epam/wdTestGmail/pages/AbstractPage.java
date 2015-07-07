61a0a6de23ebbe1120bb6382c7e2342e9e651b42
YauhenTumashchyk <Yauhen_Tumashchyk@epam.com>
2015-07-07 17:35:49 +0300

    first test commit

---
+++ src/test/java/com/epam/wdTestGmail/pages/AbstractPage.java
@@ -0,0 +1,16 @@
+package com.epam.wdTestGmail.pages;
+
+
+import org.openqa.selenium.WebDriver;
+
+public abstract class AbstractPage {
+
+    protected WebDriver driver;
+
+    public abstract void openPage();
+
+    public AbstractPage(WebDriver driver)
+    {
+        this.driver = driver;
+    }
+}
